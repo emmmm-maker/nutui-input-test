58486f995b9ff50f3684c34ef1e8137805a35ef9
xingchaochao <xing_chaochao@sina.com>
2023-03-01 15:30:08 +0800

    firstCommit

---
+++ src/pages/index/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <nut-input v-model="id" placeholder="请输入身份证" type="idcard" />
+  <nut-button type='primary' @click="getId">获取身份证</nut-button>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const id = ref<string>('')
+
+const getId = () => {
+  console.log('id', id.value);
+}
+</script>
+
+<style lang="scss">
+.index {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+</style>
